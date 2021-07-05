064ae51c7d091b96fcdfd6ed19e249dd67e9145e
Elize <delabrida.elize@hotmail.com>
2021-07-05 14:21:41 -0300

    Refactor adapter test

--- app/src/test/java/com/elize/countries/ui/adapter/CountryListAdapterTest.kt
+++ app/src/test/java/com/elize/countries/ui/adapter/CountryListAdapterTest.kt
@@ -1,37 +1,51 @@
 package com.elize.countries.ui.adapter
 
 import com.elize.countries.fixtures.countryList
-import junit.framework.Assert.assertEquals
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+import org.mockito.Spy
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-@Config(manifest= Config.NONE)
+@Config(manifest = Config.NONE)
 class CountryListAdapterTest {
 
-    private val adapter : CountryListAdapter = CountryListAdapter(arrayListOf())
+    @Spy
+    lateinit var adapter: CountryListAdapter
+
+    @Before
+    fun setup() {
+        adapter = CountryListAdapter(countryList)
+        MockitoAnnotations.openMocks(this)
+    }
 
     @Test
     fun `should return correct size`() {
         adapter.updateCountries(countryList)
 
-        assertEquals(countryList.size, adapter.itemCount)
+        assertThat(adapter.itemCount, equalTo(countryList.size))
     }
 
     @Test
     fun `should update countries`() {
         adapter.updateCountries(countryList)
 
-        assertEquals(countryList, adapter.countries)
+        verify(adapter).notifyDataSetChanged()
+        assertThat(countryList, equalTo(adapter.countries))
     }
 
     @Test
     fun `should clear items`() {
         adapter.clearAllItems()
 
-        assertEquals(0, adapter.itemCount)
+        verify(adapter).notifyDataSetChanged()
+        assertThat(0, equalTo(adapter.itemCount))
     }
 
 }
